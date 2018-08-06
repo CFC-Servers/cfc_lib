413fc85292f112f8362c1f41b00ed21a90281da9
Justin Muskopf <jaymuskopf@gmail.com>
2018-08-06 10:44:04 -0500

    Add isValidPlayer function

--- lua/cfclib/cfc_lib_general.lua
+++ lua/cfclib/cfc_lib_general.lua
@@ -9,4 +9,8 @@ function CFCLib.generateCFCHook(hookName)
 	return cfcHookPrefix .. hookName
 end
 
+function CFCLib.isValidPlayer(ply)
+    return IsValid( ply ) and ply:IsPlayer()
+end
+
 -------------------------------------------------
